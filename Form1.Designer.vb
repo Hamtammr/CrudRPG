5daad3293a68de97a7c9fbb9c209adaaedf383bc
Mario E <mariodelycan@gmail.com>
2021-09-04 02:33:12 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Save = New System.Windows.Forms.Button()
+        Me.dGrid = New System.Windows.Forms.DataGridView()
+        Me.Edit = New System.Windows.Forms.Button()
+        Me.txtPesquisar = New System.Windows.Forms.Button()
+        Me.Exibir = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtClasse = New System.Windows.Forms.TextBox()
+        Me.txtPersonagem = New System.Windows.Forms.TextBox()
+        Me.txtJogador = New System.Windows.Forms.TextBox()
+        Me.txtIDJogador = New System.Windows.Forms.TextBox()
+        Me.btnDeletar = New System.Windows.Forms.Button()
+        CType(Me.dGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Save
+        '
+        Me.Save.Location = New System.Drawing.Point(15, 193)
+        Me.Save.Name = "Save"
+        Me.Save.Size = New System.Drawing.Size(117, 45)
+        Me.Save.TabIndex = 0
+        Me.Save.Text = "Salvar"
+        Me.Save.UseVisualStyleBackColor = True
+        '
+        'dGrid
+        '
+        Me.dGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dGrid.Location = New System.Drawing.Point(12, 244)
+        Me.dGrid.Name = "dGrid"
+        Me.dGrid.Size = New System.Drawing.Size(776, 194)
+        Me.dGrid.TabIndex = 2
+        '
+        'Edit
+        '
+        Me.Edit.Location = New System.Drawing.Point(261, 193)
+        Me.Edit.Name = "Edit"
+        Me.Edit.Size = New System.Drawing.Size(117, 45)
+        Me.Edit.TabIndex = 3
+        Me.Edit.Text = "Editar"
+        Me.Edit.UseVisualStyleBackColor = True
+        '
+        'txtPesquisar
+        '
+        Me.txtPesquisar.Location = New System.Drawing.Point(138, 193)
+        Me.txtPesquisar.Name = "txtPesquisar"
+        Me.txtPesquisar.Size = New System.Drawing.Size(117, 45)
+        Me.txtPesquisar.TabIndex = 4
+        Me.txtPesquisar.Text = "Pesquisar"
+        Me.txtPesquisar.UseVisualStyleBackColor = True
+        '
+        'Exibir
+        '
+        Me.Exibir.Location = New System.Drawing.Point(384, 193)
+        Me.Exibir.Name = "Exibir"
+        Me.Exibir.Size = New System.Drawing.Size(117, 45)
+        Me.Exibir.TabIndex = 5
+        Me.Exibir.Text = "Exibir"
+        Me.Exibir.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 29)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "ID"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 58)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Jogador"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 84)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Personagem"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 110)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Classe"
+        '
+        'txtClasse
+        '
+        Me.txtClasse.Location = New System.Drawing.Point(94, 107)
+        Me.txtClasse.Name = "txtClasse"
+        Me.txtClasse.Size = New System.Drawing.Size(138, 20)
+        Me.txtClasse.TabIndex = 10
+        '
+        'txtPersonagem
+        '
+        Me.txtPersonagem.Location = New System.Drawing.Point(94, 81)
+        Me.txtPersonagem.Name = "txtPersonagem"
+        Me.txtPersonagem.Size = New System.Drawing.Size(138, 20)
+        Me.txtPersonagem.TabIndex = 11
+        '
+        'txtJogador
+        '
+        Me.txtJogador.Location = New System.Drawing.Point(94, 55)
+        Me.txtJogador.Name = "txtJogador"
+        Me.txtJogador.Size = New System.Drawing.Size(138, 20)
+        Me.txtJogador.TabIndex = 12
+        '
+        'txtIDJogador
+        '
+        Me.txtIDJogador.Location = New System.Drawing.Point(94, 26)
+        Me.txtIDJogador.Name = "txtIDJogador"
+        Me.txtIDJogador.Size = New System.Drawing.Size(57, 20)
+        Me.txtIDJogador.TabIndex = 13
+        '
+        'btnDeletar
+        '
+        Me.btnDeletar.Location = New System.Drawing.Point(671, 193)
+        Me.btnDeletar.Name = "btnDeletar"
+        Me.btnDeletar.Size = New System.Drawing.Size(117, 45)
+        Me.btnDeletar.TabIndex = 14
+        Me.btnDeletar.Text = "Deletar"
+        Me.btnDeletar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnDeletar)
+        Me.Controls.Add(Me.txtIDJogador)
+        Me.Controls.Add(Me.txtJogador)
+        Me.Controls.Add(Me.txtPersonagem)
+        Me.Controls.Add(Me.txtClasse)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Exibir)
+        Me.Controls.Add(Me.txtPesquisar)
+        Me.Controls.Add(Me.Edit)
+        Me.Controls.Add(Me.dGrid)
+        Me.Controls.Add(Me.Save)
+        Me.Name = "Form1"
+        Me.Text = "CrudRPG"
+        CType(Me.dGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Save As Button
+    Friend WithEvents dGrid As DataGridView
+    Friend WithEvents Edit As Button
+    Friend WithEvents txtPesquisar As Button
+    Friend WithEvents Exibir As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtClasse As TextBox
+    Friend WithEvents txtPersonagem As TextBox
+    Friend WithEvents txtJogador As TextBox
+    Friend WithEvents txtIDJogador As TextBox
+    Friend WithEvents btnDeletar As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,142 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private conexao As MySqlConnection
+    Private comando As MySqlCommand
+    Private da As MySqlDataAdapter
+    Private dr As MySqlDataReader
+    Private strSQL As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Save.Click
+        Try
+
+            conexao = New MySqlConnection("Server=localhost;Database=crudrpg;Uid=root;Pwd=meuteste;")
+            strSQL = "INSERT INTO players (Jogador,Personagem,Classe) VALUES (@Jogador, @Personagem, @Classe)"
+
+            comando = New MySqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@Jogador", txtJogador.Text)
+            comando.Parameters.AddWithValue("@Personagem", txtPersonagem.Text)
+            comando.Parameters.AddWithValue("@Classe", txtClasse.Text)
+
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            conexao = Nothing
+            comando = Nothing
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Edit.Click
+        Try
+
+            conexao = New MySqlConnection("Server=localhost;Database=crudrpg;Uid=root;Pwd=meuteste;")
+            strSQL = "UPDATE players SET Jogador=@Jogador, Personagem=@Personagem, Classe=@Classe WHERE IDJogador=@IDJogador"
+
+            comando = New MySqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@IDJogador", txtIDJogador.Text)
+            comando.Parameters.AddWithValue("@Jogador", txtJogador.Text)
+            comando.Parameters.AddWithValue("@Personagem", txtPersonagem.Text)
+            comando.Parameters.AddWithValue("@Classe", txtClasse.Text)
+
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            conexao = Nothing
+            comando = Nothing
+
+        End Try
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Exibir.Click
+        Try
+
+            conexao = New MySqlConnection("Server=localhost;Database=crudrpg;Uid=root;Pwd=meuteste;")
+            strSQL = "SELECT * FROM Players"
+
+            Dim dt As New DataTable
+
+            da = New MySqlDataAdapter(strSQL, conexao)
+
+            da.Fill(dt)
+
+            dGrid.Datasource = dt
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            conexao = Nothing
+            comando = Nothing
+
+        End Try
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnDeletar.Click
+        Try
+
+            conexao = New MySqlConnection("Server=localhost;Database=crudrpg;Uid=root;Pwd=meuteste;")
+            strSQL = "DELETE FROM players WHERE IDJogador=@IDJogador"
+
+            comando = New MySqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@IDJogador", txtIDJogador.Text)
+
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            conexao = Nothing
+            comando = Nothing
+
+        End Try
+    End Sub
+
+    Private Sub txtPesquisar_Click(sender As Object, e As EventArgs) Handles txtPesquisar.Click
+        Try
+
+            conexao = New MySqlConnection("Server=localhost;Database=crudrpg;Uid=root;Pwd=meuteste;")
+            strSQL = "SELECT * FROM Players WHERE IDJogador=@IDJogador"
+
+            comando = New MySqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@IDJogador", txtIDJogador.Text)
+            comando.Parameters.AddWithValue("@Jogador", txtJogador.Text)
+            comando.Parameters.AddWithValue("@Personagem", txtPersonagem.Text)
+            comando.Parameters.AddWithValue("@Classe", txtClasse.Text)
+
+            conexao.Open()
+            dr = comando.ExecuteReader()
+
+            Do While dr.Read
+                txtJogador.Text = dr("Jogador")
+                txtPersonagem.Text = dr("Personagem")
+                txtClasse.Text = dr("Classe")
+
+            Loop
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            conexao = Nothing
+            comando = Nothing
+
+        End Try
+    End Sub
+End Class
